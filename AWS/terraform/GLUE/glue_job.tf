d2791915693f6029a01e6b3a23e18ef71def935e
Fırat Melih Yılmaz <41309751+fmyilmaz@users.noreply.github.com>
2024-04-22 00:30:43 +0300

    Add AWS Glue job for raw data processing

--- AWS/terraform/GLUE/glue_job.tf
+++ AWS/terraform/GLUE/glue_job.tf
@@ -15,4 +15,31 @@ provider "aws" {
 }
 
 # resource block
-
+resource "aws_glue_job" "glue_job" {
+    name = "raw_data_processing_job" # name of the glue job
+    description = "gather data from source and process it" # description of the glue job
+    role_arn = aws_iam_role.glue_service_role.arn # arn of the glue service role
+    command {
+        name = "glueetl" # name of the command
+        script_location = "s3://aws-glue-assets-471112935345-us-east-1/scripts/glue_job.py" # location of the script
+    }
+    default_arguments = {
+        "--job-language" = "python" # job language
+        "--job-bookmark-option" = "job-bookmark-enable" # job bookmark option
+    }
+    execution_property {
+        max_concurrent_runs = 1 # maximum concurrent runs
+    }
+    glue_version = "4.0" # glue version
+    max_capacity = 0.0625 # maximum capacity
+    max_retries = 0 # maximum retries
+    worker_type = "G.1X" # worker type
+    number_of_workers = 2 # number of workers
+    security_configuration = "" # security configuration
+    tags = {
+        Name = "glue-job" # name of the glue job
+        created_by = "firatmelihyilmaz" # created by terraform
+        environment = "production" # environment name
+    }
+  
+}
